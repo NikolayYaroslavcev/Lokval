51081ac48016c82ebad9502a9ef781179f4ab1b5
Nikolay <sweeterthanfurby@mail. git config --global user.email sweeterthanfurby@mail>
2023-03-21 13:29:46 +0300

    fill: services

--- src/components/pages/home/services/Services.tsx
+++ src/components/pages/home/services/Services.tsx
@@ -1,136 +1,135 @@
 import React from 'react';
 import {
-    Block, NavBlock,
-    ServicesCart, ServicesFlex,
+    ServicesGrid, ServicesItem,
     ServicesWrapper
-} from "./services-style";
-import {StyledContainer} from "../../../layout/header/header-style";
-import {Link} from "react-router-dom";
+} from './services-style';
+import {StyledContainer} from '../../../layout/header/header-style';
+import {Link} from 'react-router-dom';
 
-const array =[
+const array = [
     {
-        path:'/images/kinds.png',
-        title:'Виды камня',
-        linksList:[
+        path: '/images/kinds.png',
+        title: 'Виды камня',
+        linksList: [
             {
-                value:"Акушинский известняк",
-                link:"/"
+                value: 'Акушинский известняк',
+                link: '/'
             },
             {
-                value:"Гелинбатанский ракушечник",
-                link:"/"
+                value: 'Гелинбатанский ракушечник',
+                link: '/'
             },
             {
-                value:"Дербенский ракушечник",
-                link:"/"
+                value: 'Дербенский ракушечник',
+                link: '/'
             },
             {
-                value:"Каякенский ракушечник",
-                link:"/"
+                value: 'Каякенский ракушечник',
+                link: '/'
             },
             {
-                value:"Левашинский известняк",
-                link:"/"
+                value: 'Левашинский известняк',
+                link: '/'
             },
             {
-                value:"Мекигинский доломит",
-                link:"/"
+                value: 'Мекигинский доломит',
+                link: '/'
             },
             {
-                value:"Рукельский ракушечник",
-                link:"/"
+                value: 'Рукельский ракушечник',
+                link: '/'
             },
             {
-                value:"Чиринский известняк",
-                link:"/"
+                value: 'Чиринский известняк',
+                link: '/'
             },
             {
-                value:"Фрикский ракушечник",
-                link:"/"
-            },
-            ]
-    },
-    {
-        path:'/images/finishing.png',
-        title:'Отделка камнем',
-        linksList:[
-            {
-                value:"Акушинский известняк",
-                link:"/"
-            },
-            {
-                value:"Гелинбатанский ракушечник",
-                link:"/"
-            },
-            {
-                value:"Дербенский ракушечник",
-                link:"/"
-            },
-            {
-                value:"Каякенский ракушечник",
-                link:"/"
-            },
-            {
-                value:"Левашинский известняк",
-                link:"/"
-            },
-            {
-                value:"Мекигинский доломит",
-                link:"/"
-            },
-            {
-                value:"Рукельский ракушечник",
-                link:"/"
-            },
-            {
-                value:"Чиринский известняк",
-                link:"/"
-            },
-            {
-                value:"Фрикский ракушечник",
-                link:"/"
+                value: 'Фрикский ракушечник',
+                link: '/'
             },
         ]
     },
     {
-        path:'/images/products.png',
-        title:'Изделия из камня',
-        linksList:[
+        path: '/images/finishing.png',
+        title: 'Отделка камнем',
+        linksList: [
             {
-                value:"Акушинский известняк",
-                link:"/"
+                value: 'Акушинский известняк',
+                link: '/'
             },
             {
-                value:"Гелинбатанский ракушечник",
-                link:"/"
+                value: 'Гелинбатанский ракушечник',
+                link: '/'
             },
             {
-                value:"Дербенский ракушечник",
-                link:"/"
+                value: 'Дербенский ракушечник',
+                link: '/'
             },
             {
-                value:"Каякенский ракушечник",
-                link:"/"
+                value: 'Каякенский ракушечник',
+                link: '/'
             },
             {
-                value:"Левашинский известняк",
-                link:"/"
+                value: 'Левашинский известняк',
+                link: '/'
             },
             {
-                value:"Мекигинский доломит",
-                link:"/"
+                value: 'Мекигинский доломит',
+                link: '/'
             },
             {
-                value:"Рукельский ракушечник",
-                link:"/"
+                value: 'Рукельский ракушечник',
+                link: '/'
             },
             {
-                value:"Чиринский известняк",
-                link:"/"
+                value: 'Чиринский известняк',
+                link: '/'
             },
             {
-                value:"Фрикский ракушечник",
-                link:"/"
+                value: 'Фрикский ракушечник',
+                link: '/'
+            },
+        ]
+    },
+    {
+        path: '/images/products.png',
+        title: 'Изделия из камня',
+        linksList: [
+            {
+                value: 'Акушинский известняк',
+                link: '/'
+            },
+            {
+                value: 'Гелинбатанский ракушечник',
+                link: '/'
+            },
+            {
+                value: 'Дербенский ракушечник',
+                link: '/'
+            },
+            {
+                value: 'Каякенский ракушечник',
+                link: '/'
+            },
+            {
+                value: 'Левашинский известняк',
+                link: '/'
+            },
+            {
+                value: 'Мекигинский доломит',
+                link: '/'
+            },
+            {
+                value: 'Рукельский ракушечник',
+                link: '/'
+            },
+            {
+                value: 'Чиринский известняк',
+                link: '/'
+            },
+            {
+                value: 'Фрикский ракушечник',
+                link: '/'
             },
         ]
     },
@@ -140,54 +139,54 @@ export const Services = () => {
     return (
         <ServicesWrapper>
             <StyledContainer>
-                <ServicesFlex>
-                    {array.map((el,index)=> {
-                        return(
-                            <ServicesCart key={index} style={{backgroundImage: `url(${el.path})`}} >
-                                <Block>
-                                    <h3>{el.title}</h3>
-                                    <NavBlock>
-                                        <ul>
-                                            {el.linksList.map((link, j) => (
-                                                <li key={j}><Link to={link.link} key={j}>{link.value}</Link></li>
-                                            ))}
-                                        </ul>
-                                    </NavBlock>
-                                </Block>
-                            </ServicesCart>
+                <ServicesGrid>
+                    {array.map((el, index) => {
+                        return (
+                            <ServicesItem key={index} style={{backgroundImage: `url(${el.path})`}}>
+                                <span></span>
+                                <div className="item__overlay">
+                                    <h3 id="person1" aria-hidden="true">{el.title}</h3>
+                                    <ul className="item__body">
+                                        {el.linksList.map((link, j) => (
+                                            <li key={j}><Link to={link.link} key={j}>{link.value}</Link></li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </ServicesItem>
                         )
                     })}
-                </ServicesFlex>
+
+                </ServicesGrid>
             </StyledContainer>
         </ServicesWrapper>
-
     );
 };
 
 
-// import React from 'react';
-// import {FinishingBlock, KindsBlock, ProductsBlock, ServicesBlocks, ServicesWrapper} from "./services-style";
-// import {StyledContainer} from "../../../layout/header/header-style";
-//
-//
 // export const Services = () => {
 //     return (
 //         <ServicesWrapper>
 //             <StyledContainer>
-//                 <ServicesBlocks>
-//                     <div>
-//                         <KindsBlock><h3>Виды камня</h3></KindsBlock>
-//                     </div>
-//                     <div>
-//                         <FinishingBlock><h3>Отделка камнем</h3></FinishingBlock>
-//                     </div>
-//                     <div>
-//                         <ProductsBlock><h3>Изделия из камня</h3></ProductsBlock>
-//                     </div>
-//                 </ServicesBlocks>
+//                 <ServicesFlex>
+//                     {array.map((el,index)=> {
+//                         return(
+//                             <ServicesCart key={index} style={{backgroundImage: `url(${el.path})`}} >
+//                                 <Block>
+//                                     <h3>{el.title}</h3>
+//                                     <NavBlock>
+//                                         <ul>
+//                                             {el.linksList.map((link, j) => (
+//                                                 <li key={j}><Link to={link.link} key={j}>{link.value}</Link></li>
+//                                             ))}
+//                                         </ul>
+//                                     </NavBlock>
+//                                 </Block>
+//                             </ServicesCart>
+//                         )
+//                     })}
+//                 </ServicesFlex>
 //             </StyledContainer>
 //         </ServicesWrapper>
 //
 //     );
 // };
-
--- src/components/pages/home/services/services-style.ts
+++ src/components/pages/home/services/services-style.ts
@@ -6,17 +6,22 @@ export const ServicesWrapper = styled.section`
   @media (max-width: 1024.98px) {
     margin-bottom: 20px;
   }
+  
+`
 
+
+export const ServicesGrid = styled.div`
+     display: flex;
+     justify-content: space-between;
+     @media (max-width: 836.98px) {
+       flex-direction: column;
+       gap: 10px;
+     }
+  
 `
-export const ServicesFlex = styled.div`
-  display: flex;
-  justify-content: space-between;
-  @media (max-width: 836.98px) {
-    flex-direction: column;
-    gap: 10px;
-  }
-`
-export const ServicesCart = styled.div`
+export const ServicesItem = styled.div`
+  position: relative;
+  overflow-y: hidden;
   width: calc((100% / 3) - 3px);
   height: 483px;
   display: flex;
@@ -24,41 +29,7 @@ export const ServicesCart = styled.div`
   justify-content: center;
   cursor: pointer;
   background-size: cover;
-  @media (max-width: 836.98px) {
-      width: 100%;
-  }
 
-  img{
-    height: 483px;
-  }
-  &:hover {
-    transition: all 0.3s ease;
-    div{
-      border-radius: 0;
-      max-height: 100%;
-      gap: 25px;
-      nav{
-        display: block;
-        max-height: 100%;
-        transition: all 0.3s ease;
-      }
-    }
-  }
-`
-
-export const Block = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  max-height: 80px;
-  background: rgba(89, 18, 20, 0.5);
-  border-radius: 50px 50px 0px 0px;
-  padding: 20px;
-  width: 100%;
-  transition: all 0.3s ease;
-  gap: 0;
 
   h3 {
     font-weight: 700;
@@ -66,16 +37,64 @@ export const Block = styled.div`
     line-height: 40px;
     color: #FFFFFF;
     text-align: center;
+    display: block;
+    background: rgba(89, 18, 20, 0.5);
+    padding: 1rem;
+    transform: translate3d(0, -100%, 0);
+    transition: transform 300ms;
+    border-radius: 50px 50px 0px 0px;
+    margin-bottom: 23px;
   }
 
-`
-export const NavBlock = styled.nav`
-  display: none;
-  ul {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
+  span {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+
+    &:hover,
+    &:focus {
+      ~ .item__overlay {
+        transform: translate3d(0, 0, 0);
+
+        h3 {
+          transform: translate3d(0, 0, 0);
+        }
+
+        .item__body {
+          opacity: 1;
+        }
+      }
+    }
   }
+}
+
+
+.item__overlay {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  position: absolute;
+  width: 100%;
+  top: 0;
+  transition: transform 300ms;
+  background: rgba(89, 18, 20, 0.5);
+  transform: translate3d(0, 100%, 0);
+}
+
+.item__body {
+  flex-grow: 1;
+  padding: 1rem;
+  opacity: 0;
+  transition: opacity 500ms 100ms;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 8px;
+
 
   a {
     font-weight: 500;
@@ -83,99 +102,111 @@ export const NavBlock = styled.nav`
     line-height: 20px;
     color: #FFFFFF;
   }
+
+  li {
+    margin: 0;
+  }
 `
 
 
+// export const ServicesCart = styled.div`
+//   width: calc((100% / 3) - 3px);
+//   height: 483px;
+//   display: flex;
+//   align-items: flex-end;
+//   justify-content: center;
+//   cursor: pointer;
+//   background-size: cover;
+//   @media (max-width: 836.98px) {
+//     width: 100%;
+//   }
+// `
 
 
+//
 // import styled from "styled-components";
 //
 //
 // export const ServicesWrapper = styled.section`
-//     margin-bottom: 90px;
+//   margin-bottom: 90px;
 //   @media (max-width: 1024.98px) {
 //     margin-bottom: 20px;
 //   }
 //
-//
 // `
-// export const ServicesBlocks = styled.div`
+// export const ServicesFlex = styled.div`
 //   display: flex;
 //   justify-content: space-between;
-//   gap: 5px;
-//   @media (max-width: 820.98px) {
-//     flex-wrap: wrap;
+//   @media (max-width: 836.98px) {
+//     flex-direction: column;
+//     gap: 10px;
+//   }
+// `
+// export const ServicesCart = styled.div`
+//   width: calc((100% / 3) - 3px);
+//   height: 483px;
+//   display: flex;
+//   align-items: flex-end;
+//   justify-content: center;
+//   cursor: pointer;
+//   background-size: cover;
+//   @media (max-width: 836.98px) {
+//     width: 100%;
 //   }
 //
+//   img{
+//     height: 483px;
+//   }
+//   &:hover {
+//     transition: all 0.3s ease;
+//     div{
+//       border-radius: 0;
+//       max-height: 100%;
+//       gap: 25px;
+//       nav{
+//         display: block;
+//         max-height: 100%;
+//         transition: all 0.3s ease;
+//       }
+//     }
+//   }
+// `
+//
+// export const Block = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   height: 100%;
+//   max-height: 80px;
+//   background: rgba(89, 18, 20, 0.5);
+//   border-radius: 50px 50px 0px 0px;
+//   padding: 20px;
+//   width: 100%;
+//   transition: all 0.3s ease;
+//   gap: 0;
 //
 //   h3 {
 //     font-weight: 700;
 //     font-size: 30px;
-//     //line-height: 40px;
-//     line-height: 135%;
+//     line-height: 40px;
 //     color: #FFFFFF;
-//     padding: 20px 45px;
-//     gap: 10px;
-//     width: 390px;
-//     height: 80px;
-//     background: rgba(89, 18, 20, 0.5);
-//     border-radius: 50px 50px 0 0;
 //     text-align: center;
-//     @media (max-width: 1126.98px) {
-//       padding: 7px 45px;
-//       line-height: 100%;
-//     }
-//     @media (max-width: 820.98px) {
-//       width: 100%;
-//       line-height: 200%;
-//     }
-//
 //   }
-//   div{
-//     height: 483px;
-//     overflow: hidden;
+//
+// `
+// export const NavBlock = styled.nav`
+//   ul {
 //     display: flex;
-//     align-items: flex-end;
-//     justify-content: center;
-//     cursor: pointer;
-//     background-size: cover;
-//     @media (min-width: 1024.98px) {
-//       background-size: 100% 100%;
-//       transition: background-size .3s ease-in;
-//       -moz-transition: background-size .3s ease-in;
-//       -web-kit-transition: background-size .3s ease-in;
-//       transition: all 0.3s;
-//     }
-//     @media (max-width: 820.98px) {
-//       display: flex;
-//       width: 100%;
-//     }
-//
-//   }
-// `
-// export const KindsBlock = styled.div`
-//   background: url("/images/kinds.png") 50%/cover no-repeat;
-//   @media (min-width: 1024.98px) {
-//     &:hover {
-//       background-size: 110% 110%
-//     }
-//   }
-// `
-//
-// export const FinishingBlock = styled.div`
-//   background: url("/images/finishing.png") no-repeat center;
-//   @media (min-width: 1024.98px) {
-//     &:hover {
-//       background-size: 110% 110%
-//     }
-//   }
-// `
-// export const ProductsBlock = styled.div`
-//   background: url("/images/products.png") no-repeat center;
-//   @media (min-width: 1024.98px) {
-//     &:hover {
-//       background-size: 110% 110%
-//     }
+//     flex-direction: column;
+//     gap: 8px;
 //   }
 //
+//   a {
+//     font-weight: 500;
+//     font-size: 15px;
+//     line-height: 20px;
+//     color: #FFFFFF;
+//   }
 // `
+
